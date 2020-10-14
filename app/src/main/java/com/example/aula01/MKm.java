e37067c548fdd9aefae11b8182617a1891f71ee4
jovanamachado <jovanaa_@outlook.com>
2020-10-14 13:39:20 -0300

    Adição da programação java de m para km

--- app/src/main/java/com/example/aula01/MKm.java
+++ app/src/main/java/com/example/aula01/MKm.java
@@ -3,6 +3,9 @@ package com.example.aula01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MKm extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MKm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_m_km);
+
+        final EditText edtM_prog = (EditText) findViewById(R.id.edtM);
+        final EditText edtKm_prog = (EditText) findViewById(R.id.edtKm);
+        Button btnConverter_prog = (Button) findViewById(R.id.btnConverter);
+
+        btnConverter_prog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float m, km;
+                m = Float.parseFloat(edtM_prog.getText().toString());
+                km = m / 1000;
+                edtKm_prog.setText(String.valueOf(km));
+            }
+        });
+
+
     }
 }
